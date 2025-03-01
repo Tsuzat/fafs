70d815ae71ca8d39c7e19f369a1468241ee18147
Tsuzat <dev.tsuzat@gmail.com>
2025-03-01 17:11:39 +0530

    add: enhance VitePWA configuration with manifest and caching settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,23 @@ export default defineConfig({
 	plugins: [
 		svelte(),
 		VitePWA({
-			registerType: 'autoUpdate'
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Fast And Flexible Search',
+				short_name: 'FAFS',
+				theme_color: '#f3f4f6', // Light mode theme color
+				background_color: '#f3f4f6',
+				icons: [
+					{
+						src: '/app_icon.svg', // Make sure this path is correct
+						sizes: '192x192',
+						type: 'image/svg+xml'
+					}
+				]
+			},
+			workbox: {
+				maximumFileSizeToCacheInBytes: 3 * 1024 * 1024 // 3 MB
+			}
 		})
 	]
 });
