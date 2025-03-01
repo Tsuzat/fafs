3ea2a419920f8d6e2d8fdddf4b48145798157d5e
Tsuzat <dev.tsuzat@gmail.com>
2025-03-01 16:56:57 +0530

    Add VitePWA plugin to Vite configuration for PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
+});
